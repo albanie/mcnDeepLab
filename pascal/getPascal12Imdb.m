f87923e32e1544524ef0d0bbf97f4782f1ac37e2
albanie <albanie@robots.ox.ac.uk>
2017-09-24 13:44:37 +0100

    clean

--- pascal/getPascal12Imdb.m
+++ pascal/getPascal12Imdb.m
@@ -1,5 +1,9 @@
 function imdb = getPascal12Imdb(opts) 
-% GETPASCAL12IMDB - load Pascal Imdb file
+% GETPASCAL12IMDB - load Pascal Imdb file, making use of the vocSetup
+% code by Sebastien Erhardt and Andrea Vedaldi
+%
+% Copyright (C) 2017 Samuel Albanie 
+% Licensed under The MIT License [see LICENSE.md for details]
 
   dataDir = fullfile(opts.dataOpts.root, 'pascal12') ;
   imdb = vocSetup('dataDir', dataDir, ...
